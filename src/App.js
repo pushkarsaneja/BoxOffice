f57e5f1b9e82a7aec6eb710a3393cace4ceb213d
pushkarsaneja <pushkarsaneja28@gmail.com>
2021-08-04 18:02:22 +0530

    added layout management and created title

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Navs from './components/Navs';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 
 const App = () => (
-  <div>
-    <Navs />
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/starred">
-        <Starred />
-      </Route>
-      <Route>This page does not exist.</Route>
-    </Switch>
-  </div>
+  <Switch>
+    <Route exact path="/">
+      <Home />
+    </Route>
+    <Route exact path="/starred">
+      <Starred />
+    </Route>
+    <Route>This page does not exist.</Route>
+  </Switch>
 );
 export default App;
