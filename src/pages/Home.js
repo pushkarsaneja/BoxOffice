f1bc5c892ece1db28d88a0352ad88f6a98f3d599
pushkarsaneja <pushkarsaneja28@gmail.com>
2021-08-05 23:44:53 +0530

    added search option

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,19 @@ import apiGet from '../misc/Config';
 const Home = () => {
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+  const isShowsSearch = searchOption === 'shows';
+
   const onInputChange = e => {
     setInput(e.target.value);
   };
+
+  const onRadioChange = e => {
+    setSearchOption(e.target.value);
+  };
+
   const onSearch = () => {
-    apiGet(`/search/shows?q=${input}`).then(result => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
     });
   };
@@ -26,7 +34,10 @@ const Home = () => {
 
     if (results && results.length > 0) {
       return results.map(element => {
-        return <div key={element.show.id}>{element.show.name}</div>;
+        if (element.show) {
+          return <div key={element.show.id}>{element.show.name}</div>;
+        }
+        return <div key={element.person.id}>{element.person.name}</div>;
       });
     }
 
@@ -37,10 +48,36 @@ const Home = () => {
     <MainPageLayout>
       <input
         type="text"
+        placeholder="Search for something"
         onChange={onInputChange}
         value={input}
         onKeyDown={onKeyDown}
       />
+
+      <div>
+        <label htmlFor="shows-search">
+          Shows
+          <input
+            id="shows-search"
+            type="radio"
+            onChange={onRadioChange}
+            value="shows"
+            checked={isShowsSearch}
+          />
+        </label>
+
+        <label htmlFor="actors-search">
+          Actors
+          <input
+            id="actors-search"
+            type="radio"
+            onChange={onRadioChange}
+            value="people"
+            checked={!isShowsSearch}
+          />
+        </label>
+      </div>
+
       <button type="button" onClick={onSearch}>
         Search
       </button>
