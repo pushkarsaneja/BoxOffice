a9820793b6967b9652cf1efd514bff121c866884
pushkarsaneja <pushkarsaneja28@gmail.com>
2021-08-09 20:20:33 +0530

    used custom hooks to save starred shows

--- src/components/show/ShowCard.js
+++ src/components/show/ShowCard.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { StyledShowCard } from './ShowCard.styled';
 
-const ShowCard = ({ id, image, name, summary }) => {
+// const ShowCard = ({ id, image, name, summary, onStarClick }) => {
+const ShowCard = ({ id, image, name, summary, onStarClick }) => {
   const summaryAsText = summary
     ? `${summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')}...`
     : 'No description';
@@ -19,7 +20,9 @@ const ShowCard = ({ id, image, name, summary }) => {
 
       <div className="btns">
         <Link to={`/show/${id}`}>Read more</Link>
-        <button type="button">Star me</button>
+        <button type="button" onClick={onStarClick}>
+          Star me
+        </button>
       </div>
     </StyledShowCard>
   );
