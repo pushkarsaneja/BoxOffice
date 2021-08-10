d979696799f51e1c994580633acfca53f42f7b4b
pushkarsaneja <pushkarsaneja28@gmail.com>
2021-08-10 12:39:27 +0530

    styled navs and title components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import { ThemeProvider } from 'styled-components';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import Show from './pages/Show';
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 const App = () => (
-  <Switch>
-    <Route exact path="/">
-      <Home />
-    </Route>
-    <Route exact path="/starred">
-      <Starred />
-    </Route>
-    <Route exact path="/show/:id">
-      <Show />
-    </Route>
-    <Route>This page does not exist.</Route>
-  </Switch>
+  <ThemeProvider theme={theme}>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <Route exact path="/show/:id">
+        <Show />
+      </Route>
+      <Route>This page does not exist.</Route>
+    </Switch>
+  </ThemeProvider>
 );
 export default App;
--- src/components/Navs.js
+++ src/components/Navs.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { NavList, LinkStyled } from './Navs.styled';
 
 const LINKS = [
   { to: '/', text: 'Home' },
   { to: '/starred', text: 'Starred' },
 ];
-const Navs = () => (
-  <div>
-    <ul>
-      {LINKS.map(val => (
-        <li key={val.to}>
-          <Link to={val.to}>{val.text}</Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const Navs = () => {
+  const { pathname } = useLocation();
+  return (
+    <div>
+      <NavList>
+        {LINKS.map(val => (
+          <li key={val.to}>
+            <LinkStyled
+              to={val.to}
+              className={val.to === pathname ? 'active' : ''}
+            >
+              {val.text}
+            </LinkStyled>
+          </li>
+        ))}
+      </NavList>
+    </div>
+  );
+};
 
 export default Navs;
